a52d70e3083877f4aeeab8200cbb07c1cee16f47
ArchLeaders <archleadership28@gmail.com>
2024-08-24 00:12:56 -0700

    Impl CLI

--- src/PchtxtToIps/Program.cs
+++ src/PchtxtToIps/Program.cs
@@ -12,17 +12,21 @@ public class Program
     {
         Console.Title = Title;
 
-        if (args.Length > 0) {
-            return;
-        }
-
         try {
+            if (args.Length > 0) {
+                ProcessCli(args);
+                return;
+            }
+
             WindowsOperations.TrySetWindowMode(WindowMode.Hidden);
             BuildAvaloniaApp()
                 .StartWithClassicDesktopLifetime(args);
         }
         catch (Exception ex) {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(ex);
+            Console.ResetColor();
+
             WindowsOperations.TrySetWindowMode(WindowMode.Visible);
             Console.WriteLine("Press any key to continue . . .");
             Console.ReadKey();
@@ -34,4 +38,29 @@ public class Program
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
+
+    private static void ProcessCli(string[] args)
+    {
+        int excludeIndex = -1;
+        string output = Directory.GetCurrentDirectory();
+
+        for (int i = 0; i < args.Length; i++) {
+            string arg = args[i];
+
+            if (arg.ToLower() is "-o" or "--output" && ++i < args.Length) {
+                excludeIndex = i - 1;
+                output = args[i];
+                continue;
+            }
+        }
+
+        for (int i = 0; i < args.Length; i++) {
+            if (i == excludeIndex || i == excludeIndex + 1) {
+                continue;
+            }
+
+            string input = args[i];
+            // TODO: convert input into output
+        }
+    }
 }
